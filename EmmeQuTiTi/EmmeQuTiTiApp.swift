40f213957a4546e4266e547e714a9a502f2aba57
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2025-12-22 18:58:07 +0100

    Initial Commit

---
+++ EmmeQuTiTi/EmmeQuTiTiApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmmeQuTiTiApp.swift
+//  EmmeQuTiTi
+//
+//  Created by Francesco Paolo Severino on 22/12/25.
+//
+
+import SwiftUI
+
+@main
+struct EmmeQuTiTiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmmeQuTiTiTests/EmmeQuTiTiTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmmeQuTiTiTests.swift
+//  EmmeQuTiTiTests
+//
+//  Created by Francesco Paolo Severino on 22/12/25.
+//
+
+import Testing
+@testable import EmmeQuTiTi
+
+struct EmmeQuTiTiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
